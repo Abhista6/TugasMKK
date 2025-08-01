21e3e38dc02ec34cf0a6a35d06a569cbabe6bcfa
Abhista6 <abhistahermawan@gmail.com>
2025-08-01 10:44:02 +0700

    Add files via upload

---
+++ buku_tamu/koneksi.php
@@ -0,0 +1,15 @@
+<?php
+// Detail koneksi database
+$servername = "localhost";
+$username   = "root";
+$password   = "";
+$dbname     = "resepsionis";
+
+// Membuat koneksi
+$conn = mysqli_connect($servername, $username, $password, $dbname);
+
+// Cek koneksi
+if (!$conn) {
+    die("Koneksi gagal: " . mysqli_connect_error());
+}
+?>
